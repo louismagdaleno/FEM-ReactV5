707e1d4ef810074f5813552d0e7836ef708f6dfa
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-17 13:14:12 -0700

    Added Pet.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -26,4 +19,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
