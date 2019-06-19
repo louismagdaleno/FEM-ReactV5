11655a809519ca8f1eb5cb26d96e3ba90806c3fd
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-19 13:01:55 -0700

    Added animal dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlfor="location">
           location
@@ -14,6 +17,19 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlfor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onchange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
